843c3843da11b15f3c28619d570b88b80d0df8e4
Sam <ss11976@nyu.edu>
2021-05-13 05:04:10 +0530

    added audio

--- final_project/sketch.js
+++ final_project/sketch.js
@@ -7,29 +7,40 @@ let homeb = document.getElementById('home');
 homeb.addEventListener('click', home);
 let studyb = document.getElementById('study');
 studyb.addEventListener('click', study);
-// let yesb = document.getElementById('yes');
-// yesb.addEventListener('click', yes);
-// let nob = document.getElementById('no');
-// nob.addEventListener('click', no);
+let yesb = document.getElementById('yes');
+yesb.addEventListener('click', yes);
+let nob = document.getElementById('no');
+nob.addEventListener('click', no);
+let stopb = document.getElementById('stop');
+stopb.addEventListener('click', stop);
 
 $(document).ready(function() { 
+  $('audio#bg')[0].play()
+  $(".one").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
   $(".one").click(function() {
     var src = $('img').attr('src');
     $("img").width('25%')
+    $('audio#in')[0].play()
     if(src == 'images/1.jpg') {
         $("img").attr("src","images/2.jpg");
     } else if(src == "images/2.jpg") {
-        $("img").attr("src","images/3.jpg"); 
+        $("img").attr("src","images/3.jpg");
     } else if(src == "images/3.jpg"){
         $("img").attr("src","images/4.jpg");
+        $(".truth").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+        $(".dare").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
     } else {$("img").attr("src","images/1.jpg");}
   });
-  // $('#b').removeClass('one').addClass('two');
 });
 
 function truth() {
   tord = "t";
+  $(".one").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".truth").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".dare").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".two").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
   $(".two").click(function() {
+    $('audio#in')[0].play()
     var src = $('img').attr('src');
     $("img").width('25%')
     if(src == 'images/4.jpg') {
@@ -38,13 +49,20 @@ function truth() {
         $("img").attr("src","images/t1.jpg"); 
     } else if(src == "images/t1.jpg"){
         $("img").attr("src","images/t2.jpg");
+        $(".home").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+        $(".study").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
     } else {$("img").attr("src","images/t.jpg");}
   }); 
 }
 
 function dare() {
   tord = "d";
+  $(".one").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".truth").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".dare").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".two").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
   $(".two").click(function() {
+    $('audio#in')[0].play()
     var src = $('img').attr('src');
     $("img").width('25%')
     if(src == 'images/4.jpg') {
@@ -53,13 +71,19 @@ function dare() {
         $("img").attr("src","images/d1.jpg"); 
     } else if(src == "images/d1.jpg"){
         $("img").attr("src","images/d2.jpg");
+        $(".home").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+        $(".study").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
     } else {$("img").attr("src","images/d.jpg");}
   }); 
 }
 
 function home() {
+  $(".a").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".switch").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".three").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
   if(tord == "t") {
     $(".three").click(function() {
+      $('audio#in')[0].play()
       var src = $('img').attr('src');
       $("img").width('25%')
       if(src == 'images/tp1.jpg') {
@@ -68,11 +92,36 @@ function home() {
           $("img").attr("src","images/tp3.jpg"); 
       } else if(src == "images/tp3.jpg"){
           $("img").attr("src","images/tp4.jpg");
+          $(".yes").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+          $(".no").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
       } else {$("img").attr("src","images/tp1.jpg");}
     }); 
   }
-  else {
-    $(".two").click(function() {
+  else if(tord == "d")  {
+    $(".three").click(function() {
+      $('audio#in')[0].play()
+      var src = $('img').attr('src');
+      $("img").width('25%')
+      if(src == 'images/dp1.jpg') {
+          $("img").attr("src","images/dp2.jpg");
+      } else if(src == "images/dp2.jpg") {
+          $("img").attr("src","images/dp3.jpg");
+      } else if(src == "images/dp3.jpg") {
+          $("img").attr("src","images/dp4.png"); 
+          $(".yes").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+          $(".no").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+      } else {$("img").attr("src","images/dp1.jpg");}
+    }); 
+  }
+}
+
+function study() {
+  $(".a").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".switch").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".three").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+  if(tord == "t") {
+    $(".three").click(function() {
+      $('audio#in')[0].play()
       var src = $('img').attr('src');
       $("img").width('25%')
       if(src == 'images/ts1.jpg') {
@@ -81,27 +130,14 @@ function home() {
           $("img").attr("src","images/ts3.jpg"); 
       } else if(src == "images/ts3.jpg"){
           $("img").attr("src","images/ts4.jpg");
+          $(".yes").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+          $(".no").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
       } else {$("img").attr("src","images/ts1.jpg");}
     }); 
   }
-}
-
-function study() {
-  if(tord == "t") {
+  else if(tord == "d"){
     $(".three").click(function() {
-      var src = $('img').attr('src');
-      $("img").width('25%')
-      if(src == 'images/dp1.jpg') {
-          $("img").attr("src","images/dp2.jpg");
-      } else if(src == "images/dp2.jpg") {
-          $("img").attr("src","images/dp3.jpg"); 
-      } else if(src == "images/dp3.jpg"){
-          $("img").attr("src","images/dp4.jpg");
-      } else {$("img").attr("src","images/dp1.jpg");}
-    }); 
-  }
-  else {
-    $(".two").click(function() {
+      $('audio#in')[0].play()
       var src = $('img').attr('src');
       $("img").width('25%')
       if(src == 'images/ds1.jpg') {
@@ -110,54 +146,69 @@ function study() {
           $("img").attr("src","images/ds3.jpg"); 
       } else if(src == "images/ds3.jpg"){
           $("img").attr("src","images/ds4.jpg");
+          $(".yes").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+          $(".no").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
       } else {$("img").attr("src","images/ds1.jpg");}
     }); 
   }
 }
 
-// function yes() {
-//   $(".four").click(function() {
-//     var src = $('img').attr('src');
-//     $("img").width('25%')
-//     if(src == 'images/y1.jpg') {
-//         $("img").attr("src","images/11.jpg");
-//     } else if(src == "images/11.jpg") {
-//         $("img").attr("src","images/12.jpg"); 
-//     } else if(src == "images/12.jpg"){
-//         $("img").attr("src","images/13.jpg");
-//     } else if(src == "images/13.jpg"){
-//         $("img").attr("src","images/14.jpg");
-//     } else if(src == "images/14.jpg"){
-//         $("img").attr("src","images/15.jpg");
-//     } else if(src == "images/15.jpg"){
-//         $("img").attr("src","images/16.jpg");
-//     } else if(src == "images/16.jpg"){
-//         $("img").attr("src","images/call.jpg");
-//     } else {$("img").attr("src","images/y1.jpg");}
-//   }); 
-// }
+function yes() {
+  $(".a").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".switch").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".four").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+  $(".four").click(function() {
+    $('audio#in')[0].play()
+    var src = $('img').attr('src');
+    $("img").width('25%')
+    if(src == 'images/y1.jpg') {
+        $("img").attr("src","images/11.jpg");
+    } else if(src == "images/11.jpg") {
+        $("img").attr("src","images/12.jpg"); 
+    } else if(src == "images/12.jpg"){
+        $("img").attr("src","images/13.jpg");
+    } else if(src == "images/13.jpg"){
+        $("img").attr("src","images/14.jpg");
+    } else if(src == "images/14.jpg"){
+        $("img").attr("src","images/15.jpg");
+    } else if(src == "images/15.jpg"){
+        $("img").attr("src","images/16.jpg");
+        $(".stop").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+    } else {$("img").attr("src","images/y1.jpg");}
+  }); 
+}
 
-// function no() {
-//   $(".four").click(function() {
-//     var src = $('img').attr('src');
-//     $("img").width('25%')
-//     if(src == 'images/n1.jpg') {
-//         $("img").attr("src","images/11.jpg");
-//     } else if(src == "images/11.jpg") {
-//         $("img").attr("src","images/12.jpg"); 
-//     } else if(src == "images/12.jpg"){
-//         $("img").attr("src","images/13.jpg");
-//     } else if(src == "images/13.jpg"){
-//         $("img").attr("src","images/14.jpg");
-//     } else if(src == "images/14.jpg"){
-//         $("img").attr("src","images/15.jpg");
-//     } else if(src == "images/15.jpg"){
-//         $("img").attr("src","images/16.jpg");
-//     } else if(src == "images/16.jpg"){
-//         $("img").attr("src","images/call.jpg");
-//     } else {$("img").attr("src","images/y1.jpg");}
-//   }); 
-// }
+function no() {
+  $(".a").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".switch").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".four").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+  $(".four").click(function() {
+    $('audio#in')[0].play()
+    var src = $('img').attr('src');
+    $("img").width('25%')
+    if(src == 'images/n1.jpg') {
+        $("img").attr("src","images/11.jpg");
+    } else if(src == "images/11.jpg") {
+        $("img").attr("src","images/12.jpg"); 
+    } else if(src == "images/12.jpg"){
+        $("img").attr("src","images/13.jpg");
+    } else if(src == "images/13.jpg"){
+        $("img").attr("src","images/14.jpg");
+    } else if(src == "images/14.jpg"){
+        $("img").attr("src","images/15.jpg");
+    } else if(src == "images/15.jpg"){
+        $("img").attr("src","images/16.jpg");
+        $(".stop").css("backgroundColor", "rgba(100, 109, 119, 0.726)");
+    } else {$("img").attr("src","images/n1.jpg");}
+  }); 
+}
+
+function stop() {
+  $('audio#call')[0].play()
+  $(".a").css("backgroundColor", "rgba(0,0,0,0)");
+  $(".switch").css("backgroundColor", "rgba(0,0,0,0)");
+  $("img").attr("src","images/call.jpg");
+}
 
 
 // $('.switch').on({
